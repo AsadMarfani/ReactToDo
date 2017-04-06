58c0cad92db5d82fc7f646420128af8b013f6d2f
Asad Marfani <Asad Marfani>
2017-04-07 01:21:11 +0500

    Added Router for Private Pages

--- app/app.jsx
+++ app/app.jsx
@@ -1,19 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
+import firebase from 'app/firebase/';
+import router from 'app/router/router';
 
-var ToDoAPI = require('ToDoAPI');
-
-import Login from 'Login';
-import ToDo from 'ToDo';
+firebase.auth().onAuthStateChanged((user)=>{
+    if(user) {
+        hashHistory.push('/todos');
+    }
+    else {
+        hashHistory.push('/');
+    }
+});
 
 var actions = require('actions');
 var store = require('configureStore').configure();
 
 store.dispatch(actions.loadToDos());
-
 //Load Foundation
 require('style-loader!css-loader!foundation-sites/dist/css/foundation.min.css');
 $(document).foundation();
@@ -23,15 +28,7 @@ require('style-loader!css-loader!sass-loader!ApplicationStyles');
 
 ReactDOM.render(
     <Provider store = {store}>
-        
-            <Router history = {hashHistory}>
-                <Route path = '/'>
-                    <IndexRoute component = {Login}></IndexRoute>
-                    <Route path = "todos" component = {ToDo}>
-                    </Route>
-                </Route>
-                
-            </Router>
+        {router}
     </Provider>,
     document.getElementById('app')
 );
